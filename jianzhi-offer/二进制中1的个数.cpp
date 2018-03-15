81e011f6413a1d50eed33fc33a01d9ce2365dcbc
AstarLight <lijunshi2015@163.com>
2018-03-15 09:42:59 +0800

    Create 二进制中1的个数.cpp

---
+++ jianzhi-offer/二进制中1的个数.cpp
@@ -0,0 +1,16 @@
+/*
+输入一个整数，输出该数二进制表示中1的个数。其中负数用补码表示。
+*/
+
+class Solution {
+public:
+     int  NumberOf1(int n) {
+         int count = 0;
+         while(n)
+         {
+            n = (n-1) & n;
+            count++;
+         }         
+         return count;
+     }
+};
