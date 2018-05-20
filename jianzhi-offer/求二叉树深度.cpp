9689a88bb6a7ab882202aed031b8ec0c9288f4b1
AstarLight <lijunshi2015@163.com>
2018-05-21 00:36:07 +0800

    Create 求二叉树深度.cpp

---
+++ jianzhi-offer/求二叉树深度.cpp
@@ -0,0 +1,50 @@
+/*
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+	TreeNode(int x) :
+			val(x), left(NULL), right(NULL) {
+	}
+};*/
+#if 0
+class Solution {
+public:
+    int TreeDepth(TreeNode* pRoot)
+    {
+        if(pRoot == NULL)
+            return 0;
+        return 1 + max(TreeDepth(pRoot->left),TreeDepth(pRoot->right));
+    }
+};
+#endif
+class Solution {
+public:
+    int TreeDepth(TreeNode* pRoot)
+    {
+        if (pRoot == NULL)
+            return 0;
+        queue<TreeNode *> q;
+        q.push(pRoot);
+        int res = 0;
+        while (!q.empty())
+        {
+            int size = q.size();
+            for (int i = 0; i < size; ++i)
+            {
+                TreeNode *tmp = q.front();
+                q.pop();
+                if (tmp->left)
+                {
+                    q.push(tmp->left);
+                }
+                if (tmp->right)
+                {
+                    q.push(tmp->right);
+                }
+            }
+            res++;
+        }
+	    return res;
+    }
+};
