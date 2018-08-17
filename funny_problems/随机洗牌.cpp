f04606a5de815c3f2fcb96803a445f91f8768605
James Lee <lijunshi2015@163.com>
2018-08-17 19:55:02 +0800

    Update 随机洗牌.cpp

--- funny_problems/随机洗牌.cpp
+++ funny_problems/随机洗牌.cpp
@@ -1,1 +1,41 @@
 
+#include<iostream>  
+#include <vector>
+
+using namespace std;
+
+int shuffle(vector<int>& vec)
+{
+    for (int i = vec.size()-1; i > 0; i--)
+    {
+        int index = rand() % (i+1);
+        swap(vec[i], vec[index]);
+    }
+    return 0;
+}
+
+
+int main()
+{
+    vector<int> test;
+    for (int i = 1; i <= 53; i++)
+    {
+        test.push_back(i);
+    }
+
+    shuffle(test);
+
+    for (int i = 0; i < test.size(); i++)
+    {
+        cout << test[i] << " ";
+        if (i % 10 == 0)
+            cout << endl;
+    }
+
+    cout << endl;
+
+
+    system("pause");
+
+    return 0;
+}
