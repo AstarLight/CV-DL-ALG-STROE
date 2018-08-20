c44218c075812f84d87ab898c1b697c6714bdda8
James Lee <lijunshi2015@163.com>
2018-08-20 22:52:25 +0800

    Update 判断素数.cpp

--- funny_problems/判断素数.cpp
+++ funny_problems/判断素数.cpp
@@ -1,1 +1,24 @@
 
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool isPrime(int n)
+{
+    for(int i=2;i<=sqrt(n);i++)
+    {
+        if(n % i == 0)
+            return false;
+    }
+    return true;
+}
+
+
+int main()
+{
+    string s;
+    isPrime(97)?s="yes":s="no";
+    cout<< s << endl;
+
+    return 0;
+}
