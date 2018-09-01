431fdda877c9e95b8d69fef6fa3355ea45defeca
James Lee <lijunshi2015@163.com>
2018-09-01 15:38:59 +0800

    Update 链表插入排序_insertion-sort-list.cpp

--- nowcoder_leetcode/链表插入排序_insertion-sort-list.cpp
+++ nowcoder_leetcode/链表插入排序_insertion-sort-list.cpp
@@ -8,6 +8,7 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+#if 0  //迭代代码我觉得有问题，因为最后一个节点的next没有指向NULL，会造成环形链表
 class Solution {
 public:
     ListNode *insertionSortList(ListNode *head) {
@@ -26,6 +27,30 @@ public:
             pre->next = p;
             p = next;
         }
+        return dumy->next;
+    }
+};
+#endif
+
+class Solution {
+public:
+    ListNode *insertionSortList(ListNode *head) {
+        if(!head || !head->next)
+            return head;
+        
+        ListNode* dumy = new ListNode(0);
+        dumy->next = insertionSortList(head->next);
+        
+        ListNode* p = dumy;
+        
+        while(p && p->next && head->val > p->next->val)
+        {
+            p = p->next;
+        }
+        
+        head->next = p->next;
+        p->next = head;
+        
         return dumy->next;
     }
 };
