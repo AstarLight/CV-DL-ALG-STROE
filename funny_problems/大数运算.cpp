ae81821b851b93f51d1aed1b1dd54a4becd12924
James Lee <lijunshi2015@163.com>
2018-09-21 17:32:04 +0800

    Update 大数运算.cpp

--- funny_problems/大数运算.cpp
+++ funny_problems/大数运算.cpp
@@ -1,5 +1,6 @@
-
 /*
+ * 大数加法，支持正数＋正数，负数＋负数
+ * 大数减法，支持大数减小数，也支持小数减大数
  * 大数乘法，支持正负数相乘
  * */
 
@@ -10,6 +11,54 @@
 #define MAX 1000    // 大数的最大位数
 
 using namespace std;
+
+void add(string num1,string num2, string& sum)
+{
+    int i, j;
+    int a[MAX+10] = {0};
+    int b[MAX+10] = {0};
+    int c[MAX+10] = {0};
+    int num1_sign = 0;
+    int num2_sign = 0;
+
+    for(j=0,i=num1.size()-1;i>=0;i--)
+    {
+        if(num1[i] == '-')  //跳过负号
+        {
+            num1_sign = 1;
+            continue;
+        }
+        a[j++] = num1[i]-'0';
+    }
+
+    for(j = 0, i = num2.size()-1; i >= 0; i--)
+    {
+        if(num2[i] == '-')  //跳过负号
+        {
+            num2_sign = 1;
+            continue;
+        }
+        b[j++] = num2[i]-'0';
+    }
+
+    int carry = 0;
+    for(i=0;i<MAX;i++)
+    {
+        int total = a[i] + b[i] + carry;
+        c[i] += total % 10;
+        carry = total/10;
+    }
+
+    for(i = MAX+10-1; c[i]==0 && i>=0; i--); //跳过高位的0
+
+    for(; i>=0; i--)
+        sum+=(c[i]+'0');
+
+    if(num1_sign && num2_sign)
+    {
+        sum = '-' + sum;
+    }
+}
 
 void mul(string num1,string num2, string& sum)
 {
@@ -61,7 +110,7 @@ void mul(string num1,string num2, string& sum)
     }
 
     for(i = MAX*2; c[i]==0 && i>=0; i--); //跳过高位的0
-    len = i+1; // 记录结果的长度
+
     for(; i>=0; i--)
         sum+=(c[i]+'0');
 
@@ -77,14 +126,16 @@ int main()
     int i, len;
     vector<char> num1;
     vector<char> num2;
-    string sum;
+    string add_sum, mul_sum;
     string s1, s2;
     cin >> s1;
     cin >> s2;
 
-    mul(s1, s2, sum);
+    mul(s1, s2, mul_sum);
+    add(s1, s2, add_sum);
 
-    cout << sum << endl;
+    cout << "add sum = " << add_sum << endl;
+    cout << "mul sum = " << mul_sum << endl;
 
     return 0;
 }
